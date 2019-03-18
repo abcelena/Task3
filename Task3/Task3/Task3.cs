8a419f2f902d542e08b4519b75246195194c4fac
Olena Moldovan <olena.moldovan@tectocom.com>
2019-03-18 10:08:44 +0100

    add solution

---
+++ Task3/Task3/Task3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Task3
+    {
+        public int solution(int[] A, int M)
+        {
+            var subsetSizes = new List<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                var subset = new int[0];
+                subsetSizes.Add(GetMAllignedSubsetSize(A, M, i, subset));
+            }
+
+            return subsetSizes.Max(x => x);
+        }
+
+        private int GetMAllignedSubsetSize(int[] A, int M, int index, int[] subset)
+        {
+            while (true)
+            {
+                var subsetSize = subset.Length;
+                if (index >= A.Length)
+                {
+                    return subsetSize;
+                }
+
+                if (subsetSize == 0)
+                {
+                    Array.Resize(ref subset, 1);
+                    subset[0] = A[index];
+                    index += 1;
+                    continue;
+                }
+
+                if (subset.Any(x => Math.Abs(A[index] - x) % M != 0))
+                {
+                    index += 1;
+                    continue;
+                }
+
+                int newSize = subset.Length + 1;
+                Array.Resize(ref subset, newSize);
+                subset[newSize - 1] = A[index];
+                index = index + 1;
+            }
+        }
+    }
+}
---
+++ Task3/UnitTests/Task3Tests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Task3Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cl = new Task3.Task3();
+            var A = new int[] { -3, -2, 1, 0, 8, 7, 1 };
+            int M = 3;
+
+
+            var actual = cl.solution(A, M);
+
+            Assert.AreEqual(4, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var cl = new Task3.Task3();
+            var A = new int[100000];
+            int M = 100000;
+            for (int i = 0; i < 100000; i++)
+            {
+                A[i] = i + 1;
+            }
+            //Act
+            var actual = cl.solution(A, M);
+            //Assert
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
